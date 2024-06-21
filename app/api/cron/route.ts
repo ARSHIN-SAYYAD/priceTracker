40aa640c34ebcd78a35e1201ffc3fe4d33687a12
ARSHIN SAYYAD <arshinsayyad@ARSHINs-Laptop.local>
2024-06-21 22:52:56 +0530

    fix error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
+
 import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
 import { connectToDB } from "@/lib/mongoose";
 import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-export const maxDuration = 30; // This function can run for a maximum of 300 seconds
+export const maxDuration = 50; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
@@ -48,7 +49,7 @@ export async function GET(request: Request) {
           product
         );
 
-        //  2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
+        // ======================== 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
         const emailNotifType = getEmailNotifType(
           scrapedProduct,
           currentProduct
